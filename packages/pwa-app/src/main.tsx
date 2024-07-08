ce5f47472790f0c289c822f522ba255eec46c303
herat130 <herat130@gmail.com>
2024-07-08 16:11:10 +0530

    adding error boundary

--- packages/pwa-app/src/main.tsx
+++ packages/pwa-app/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
