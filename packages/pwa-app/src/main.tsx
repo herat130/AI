e8d0efb106e9aaa8adfdba188b8df37ccc6f5bad
herat130 <herat130@gmail.com>
2024-09-19 12:29:43 +0530

    fix: router issue on github pages

--- packages/pwa-app/src/main.tsx
+++ packages/pwa-app/src/main.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ErrorBoundary } from "react-error-boundary";
 import { ThemeProvider } from "./utils/context/themeContext/themeContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<div>Something went wrong</div>}>
-      <BrowserRouter>
+      <HashRouter>
         <ThemeProvider>
           <App />
         </ThemeProvider>
-      </BrowserRouter>
+      </HashRouter>
     </ErrorBoundary>
   </React.StrictMode>
 );
