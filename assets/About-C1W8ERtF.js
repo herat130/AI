d649f91a6315ab6992de45be1096ae1659f2a4f8
herat130 <herat130@gmail.com>
2024-09-19 12:30:00 +0530

    Updates

---
+++ assets/About-C1W8ERtF.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-E8uOxzxF.js";function r(){return t.jsx("div",{children:t.jsx("h2",{children:"This is About page"})})}export{r as default};
---
+++ assets/PageNotFound-BTiJyBEX.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-E8uOxzxF.js";function n(){return o.jsx("div",{children:"Page not found"})}export{n as default};
