79e43d5cd6a455d2b05c882fd250b5f477090353
herat130 <herat130@gmail.com>
2024-10-04 16:34:31 +0530

    sample

---
+++ packages/pwa-app/src/utils/factories/albumFactory.ts
@@ -0,0 +1,18 @@
+type Album = {
+  title: string;
+  id: number;
+};
+function albumsFactory(data: Album[]) {
+  if (!data) {
+    return [];
+  }
+  return data.map((item) => {
+    return {
+      title: item.title,
+      id: item.id,
+    };
+  });
+}
+
+export { albumsFactory };
+export type { Album };
